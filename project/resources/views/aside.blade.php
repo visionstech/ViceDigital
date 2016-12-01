0892e4657ba04a55daf9e1a878dc7cb4e3006308
Visions <odesk.visions@gmail.com>
2016-12-01 18:47:09 +0530

    up code

--- project/resources/views/aside.blade.php
+++ project/resources/views/aside.blade.php
@@ -9,15 +9,9 @@
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
                 <ul class="nav side-menu">
-                  <li><a href="{{ url('/dashboard/dashboard') }}"><i class="fa fa-home"></i>Dashboard</a></li>
+                  <li><a href="{{ url('/dashboard') }}"><i class="fa fa-home"></i>Dashboard</a></li>
                   <li><a href="{{ url('/dashboard/configuration') }}"><i class="fa fa-cog"></i> Configuration</a></li>
-                  <?php  
-                    $class='';
-                      /*if(strpos($_SERVER['REQUEST_URI'],"publisher") != false){
-                      echo "sdfsdfsd";
-                      }*/
-                  ?>
-                  <li class="{{ (strpos($_SERVER['REQUEST_URI'],'publisher') != false)?'current-page':'' }}"><a href="{{ url('/publisher/publishers') }}"><i class="fa fa-newspaper-o"></i> Publishers</a></li>
+                 <li class="{{ (strpos($_SERVER['REQUEST_URI'],'publisher') != false)?'current-page':'' }}"><a href="{{ url('/publisher/publishers') }}"><i class="fa fa-newspaper-o"></i> Publishers</a></li>
                 </ul>
               </div>
 
