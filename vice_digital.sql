c6e791116f7bf8b1137a789949764bb45f9c5d02
Visions <odesk.visions@gmail.com>
2016-12-14 17:56:48 +0530

    up db

--- vice_digital.sql
+++ vice_digital.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 14, 2016 at 12:10 PM
+-- Generation Time: Dec 14, 2016 at 05:52 PM
 -- Server version: 5.5.35-1ubuntu1
 -- PHP Version: 5.5.9-1ubuntu4.20
 
@@ -43,24 +43,7 @@ CREATE TABLE IF NOT EXISTS `ads` (
   `created_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
-
---
--- Dumping data for table `ads`
---
-
-INSERT INTO `ads` (`id`, `status`, `publisher_id`, `slotname`, `container`, `positioning`, `mobile_sizes`, `tablet_sizes`, `desktop_sizes`, `lazyload`, `page_type`, `updated_by`, `updated_ip`, `created_at`, `updated_at`) VALUES
-(9, 'Paused', 31, 'slotss', 'slot container', 'After', 'default', '250', 'default', 1, 'a:3:{i:0;s:1:"1";i:1;s:1:"2";i:2;s:1:"3";}', 2, '127.0.0.1', '2016-12-05 06:45:18', '2016-12-05 01:15:18'),
-(10, 'Live', 32, 'PositionS', 'ContainerS', 'After', '4343', 'default', '234234', 1, 'a:3:{i:0;s:1:"1";i:1;s:1:"2";i:2;s:1:"3";}', NULL, NULL, '2016-12-05 09:23:57', '2016-12-05 03:53:44'),
-(11, 'Suspended', 33, 'adminslot', 'adminContainer', 'Prepend', '899', 'default', 'default', 1, 'a:2:{i:0;s:1:"2";i:1;s:1:"3";}', 3, '127.0.0.1', '2016-12-05 06:07:28', '2016-12-05 00:37:28'),
-(12, 'Live', 36, 'developerSlot', 'developer container', 'Prepend', 'default', '', 'default', 1, 'a:1:{i:0;s:1:"2";}', NULL, NULL, NULL, NULL),
-(13, 'Deleted', 46, 'newslot', 'newcontainer', 'Append', '456', 'default', 'default', 1, 'a:2:{i:0;s:1:"2";i:1;s:1:"3";}', NULL, NULL, '2016-12-07 06:23:43', '2016-12-07 00:53:43'),
-(14, 'Live', 33, 'SecondSlot', 'SecContainer', 'Prepend', 'default', 'default', 'default', 1, 'a:1:{i:0;s:1:"2";}', NULL, NULL, NULL, NULL),
-(15, 'Live', 57, 'retestSlot', 'retestContainer', 'Prepend', 'default', '220', 'default', 1, 'a:1:{i:0;s:1:"2";}', NULL, NULL, NULL, NULL),
-(16, 'Live', 57, 'retestSlots', 'developer containers', 'Append', 'default', 'default', 'default', 1, 'a:1:{i:0;s:1:"2";}', NULL, NULL, NULL, NULL),
-(17, 'Live', 58, 'newtable', 'newtable', 'Append', 'default', '220', 'default', 1, 'a:2:{i:0;s:1:"2";i:1;s:1:"3";}', NULL, NULL, NULL, NULL),
-(18, 'Live', 58, 'developer Slots', 'developer containers', 'After', 'default', 'default', 'default', 0, 'a:1:{i:0;s:1:"1";}', NULL, NULL, NULL, NULL),
-(19, 'Live', 58, 'developer Slots', 'developer containers', 'After', 'default', 'default', 'default', 0, 'a:1:{i:0;s:1:"1";}', 3, '127.0.0.1', '2016-12-09 12:10:44', '2016-12-09 06:40:44');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -79,15 +62,7 @@ CREATE TABLE IF NOT EXISTS `adsposition_targetings` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=74 ;
-
---
--- Dumping data for table `adsposition_targetings`
---
-
-INSERT INTO `adsposition_targetings` (`id`, `publisher_id`, `ads_id`, `key`, `value`, `updated_by`, `updated_ip`, `created_at`, `updated_at`) VALUES
-(70, 58, 19, 'ss', 'ss', 3, '127.0.0.1', NULL, '2016-12-09 06:40:44'),
-(73, 58, 19, 'e', 'e', 3, '127.0.0.1', NULL, '2016-12-09 06:40:44');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -200,29 +175,7 @@ CREATE TABLE IF NOT EXISTS `publishers` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=60 ;
-
---
--- Dumping data for table `publishers`
---
-
-INSERT INTO `publishers` (`id`, `user_id`, `website`, `status`, `email`, `name`, `adunit_id`, `comscore_id`, `krux_id`, `overlays`, `infusion`, `dynamic_ads`, `programmatic`, `custom_scripting`, `updated_by`, `updated_ip`, `created_at`, `updated_at`) VALUES
-(38, 3, 'geetika', 'Live', 'geetika@test.com', 'geetikas', '3', '4', '3', 0, 0, 0, 0, NULL, 3, '127.0.0.1', NULL, '2016-12-13 23:07:49'),
-(53, 3, 'gfgfgfgf', 'Live', 'fgfgfg@fg.oo', 'fgfgfgfg', 'd', 'e', 'e', 0, 1, 0, 1, NULL, 0, NULL, NULL, NULL),
-(52, 3, 'sssssss', 'Live', 'rajeevddd@test.com', 'sdfsdfss', '3', '5', '4', 0, 1, 1, 0, NULL, 0, NULL, NULL, NULL),
-(49, 3, 'dsfsdf', 'Live', 'sdfsd@sdf.oo', 'sdfsdf', NULL, NULL, NULL, 1, 0, 0, 0, NULL, 0, NULL, '2016-12-07 06:17:23', '2016-12-07 06:17:23'),
-(33, 3, 'geetikas', 'Live', 'admin@test.com', 'Admin', '25', '88', '6', 0, 0, 1, 0, 'Admin Scripting 2\r\n\r\n\r\n', 3, '127.0.0.1', NULL, '2016-12-06 05:29:23'),
-(51, 21, 'sdfsd', 'Live', 'sdfdfsd@sdfsdf.pp', 'fsdfsd', NULL, NULL, NULL, 0, 0, 0, 0, NULL, 0, NULL, '2016-12-08 01:26:45', '2016-12-08 01:26:45'),
-(50, 3, 'sdfsdf', 'Live', 'sdfsdff@dfgdfg.oo', 'sdfsdfsd', NULL, NULL, NULL, 0, 0, 0, 0, NULL, 0, NULL, '2016-12-08 00:51:16', '2016-12-08 00:51:16'),
-(31, 2, 'rajeevDomain', 'Live', 'rajeev@test.com', 'rajeev', '24', '46', '43', 1, 1, 1, 0, 'This is custom code\r\nYou can write custom code here\r\n', 2, '127.0.0.1', NULL, '2016-12-05 01:15:29'),
-(46, 13, 'ChildDomainNew', 'Live', 'new@gmail.com', 'childcontentnew', '3', '5', '4', 1, 0, 1, 0, 'This is new custom scripting\r\n', 13, '127.0.0.1', NULL, '2016-12-09 03:34:11'),
-(45, 3, 'AdminChildDomain', 'Live', 'child@gmail.com', 'child', NULL, NULL, NULL, 1, 0, 0, 0, NULL, 3, '127.0.0.1', '2016-12-06 04:14:46', '2016-12-09 03:34:11'),
-(54, 2, 'ssssssssssss', 'Live', 'sssss@dfg.oo', 'sssssssss', NULL, NULL, NULL, 1, 0, 1, 0, NULL, 0, NULL, NULL, NULL),
-(55, 2, 'ssssssssssss', 'Live', 'sssss@dfg.oo', 'sssssssss', NULL, NULL, NULL, 1, 0, 1, 0, NULL, 0, NULL, NULL, NULL),
-(56, 2, 'sfdfsfs', 'Live', 'dfsdfs@dfg.oop', 'dfsdf', NULL, NULL, NULL, 1, 0, 0, 0, NULL, 0, NULL, NULL, NULL),
-(57, 3, 'This is retest unit', 'Live', 'retest@gmail.com', 'retest', '85', '77', '99', 1, 0, 0, 1, NULL, 0, NULL, NULL, NULL),
-(58, 3, 'NewPublishernewtable', 'Live', 'newtable@gmail.com', 'newtable', '8', '77', '9', 1, 0, 1, 1, NULL, 0, NULL, NULL, NULL),
-(59, 3, 'ssaaaaaaaaa', 'Live', 'aswsssssss@nhy.oo', 'ssssssaaaaaa', '2', '4', '3', 1, 0, 0, 0, NULL, 0, NULL, NULL, NULL);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -240,27 +193,7 @@ CREATE TABLE IF NOT EXISTS `publisher_pagetypes` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=48 ;
-
---
--- Dumping data for table `publisher_pagetypes`
---
-
-INSERT INTO `publisher_pagetypes` (`id`, `publisher_id`, `title`, `selector`, `updated_by`, `updated_ip`, `created_at`, `updated_at`) VALUES
-(28, 31, 'testp', 'Homepage', 2, '127.0.0.1', NULL, '2016-12-05 01:15:30'),
-(29, 31, 'testpe', 'Home15', 2, '127.0.0.1', NULL, '2016-12-05 01:15:30'),
-(30, 31, 'Page12', 'Homepage1', 2, '127.0.0.1', NULL, '2016-12-05 01:15:30'),
-(31, 32, 'Title34', 'Page6', 0, NULL, NULL, '2016-12-02 06:14:46'),
-(32, 33, 'testp', 'Homepage', 3, '127.0.0.1', NULL, '2016-12-05 00:22:21'),
-(33, 33, 'Title', 'Page', 3, '127.0.0.1', NULL, '2016-12-05 00:22:21'),
-(40, 38, 'we', 'we', 3, '127.0.0.1', NULL, '2016-12-13 23:07:49'),
-(41, 46, 'Title5', 'Homepage', 0, NULL, NULL, NULL),
-(42, 52, 'r', 'r', 0, NULL, NULL, NULL),
-(43, 53, 'c', 'v', 0, NULL, NULL, NULL),
-(44, 57, '77', '88', 0, NULL, NULL, NULL),
-(45, 57, '99', '66', 0, NULL, NULL, NULL),
-(46, 58, '58', '88', 0, NULL, NULL, NULL),
-(47, 59, '7', '8', 0, NULL, NULL, NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -278,38 +211,7 @@ CREATE TABLE IF NOT EXISTS `publisher_targetings` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=72 ;
-
---
--- Dumping data for table `publisher_targetings`
---
-
-INSERT INTO `publisher_targetings` (`id`, `publisher_id`, `key`, `value`, `updated_by`, `updated_ip`, `created_at`, `updated_at`) VALUES
-(37, 31, '45', '34', 2, '127.0.0.1', NULL, '2016-12-05 01:15:29'),
-(38, 31, '346', '346', 2, '127.0.0.1', NULL, '2016-12-05 01:15:30'),
-(39, 31, '345', '455', 2, '127.0.0.1', NULL, '2016-12-05 01:15:30'),
-(43, 32, 'Target1', 'Value1', 0, NULL, NULL, '2016-12-02 06:14:46'),
-(46, 32, 'target2', 'Value2', 0, NULL, NULL, '2016-12-02 06:14:46'),
-(47, 33, 're', 're', 3, '127.0.0.1', NULL, '2016-12-05 00:22:21'),
-(48, 33, '4', '5', 3, '127.0.0.1', NULL, '2016-12-05 00:22:21'),
-(49, 33, '5', '6', 3, '127.0.0.1', NULL, '2016-12-05 00:22:21'),
-(54, 36, '3', '34', 0, NULL, NULL, NULL),
-(56, 38, 'dfe', 'ewe', 3, '127.0.0.1', NULL, '2016-12-13 23:07:49'),
-(57, 38, 'wwe', 'wwe', 3, '127.0.0.1', NULL, '2016-12-13 23:07:49'),
-(58, 46, '5', '65', 0, NULL, NULL, NULL),
-(59, 46, '55', '655', 0, NULL, NULL, NULL),
-(60, 52, 'd', 'd', 0, NULL, NULL, NULL),
-(61, 52, 'f', 'f', 0, NULL, NULL, NULL),
-(62, 53, 'd', 'd', 0, NULL, NULL, NULL),
-(63, 53, 'c', 'c', 0, NULL, NULL, NULL),
-(64, 57, '89', '96', 0, NULL, NULL, NULL),
-(65, 57, '88', '55', 0, NULL, NULL, NULL),
-(66, 57, 'ss', 'ss', 0, NULL, NULL, NULL),
-(67, 57, 'dd', 'ddd', 0, NULL, NULL, NULL),
-(68, 57, 'ff', 'fff', 0, NULL, NULL, NULL),
-(69, 58, '8', '9', 0, NULL, NULL, NULL),
-(70, 58, '7', '77', 0, NULL, NULL, NULL),
-(71, 59, '5', '6', 0, NULL, NULL, NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -360,15 +262,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `status`, `remember_token`, `updated_by`, `updated_ip`, `created_at`, `updated_at`) VALUES
-(1, 'geetikas', 'geetika@test.com', '$2y$10$4LH4pUICXqr8kd3drNsIZOd0kzOYouzExeas9rMmNA59QIyc37MDq', 2, 'Live', 'nTYx16JyWQY6Wth1N7EbqKXCkaLwMhgfn1EdnEuPPs8T8b8PmzKbKPpgLxAu', 3, '127.0.0.1', '2016-11-29 04:02:42', '2016-12-06 23:39:15'),
 (2, 'Rajeev', 'rajeev@test.com', '$2y$10$XDFtZWWu5TDQWjrd5qgfsul01g7xygeVolTnuUYfPaNq6yUsoN.PW', 2, 'Live', 'tdtK57j0v6TKZh6CTeg6pR3C2c6nySsGt48JIUiEc9hUgvILcfqkNxeprRUG', 0, NULL, '2016-11-29 08:21:29', '2016-12-13 06:53:58'),
 (3, 'Admin', 'admin@test.com', '$2y$10$XDFtZWWu5TDQWjrd5qgfsul01g7xygeVolTnuUYfPaNq6yUsoN.PW', 1, 'Live', 'LZkBZqlvGCStOV9VhpQa4mIdvdhPBAvZCr9ZYUsPlYAYFeDS2rm76MIVAM2t', 3, '127.0.0.1', '2016-11-29 08:21:29', '2016-12-12 02:14:10'),
-(13, 'child', 'child@gmail.com', '$2y$10$XDFtZWWu5TDQWjrd5qgfsul01g7xygeVolTnuUYfPaNq6yUsoN.PW', 3, 'Live', 'kH0if125330yKcoUqj31EkbBUComAC375VBWZEDBZmBRtJCgjhTtcw0gPr9k', 3, '127.0.0.1', '2016-12-06 04:14:46', '2016-12-11 23:46:50'),
-(19, 'sdfsdf', 'sdfsdf@dfgg.oo', '$2y$10$bnTc.yWVFHvb9E23GG/H5O2qOXEVW56EaRvE1AKLrxO6P5c.kXMoy', 2, 'Live', NULL, 0, NULL, '2016-12-08 00:49:59', '2016-12-08 00:49:59'),
-(18, 'sdfsdff', 'sdfsdf@dfg.oo', '$2y$10$1zsWcCTDBVLGvMFWkAEMKueZg0CITHR8Enr/Zyi2pYQmgtm2F7GBG', 1, 'Live', NULL, 0, NULL, '2016-12-07 06:19:36', '2016-12-07 06:19:36'),
-(17, 'sdfsdf', 'sdfsd@sdf.oo', '$2y$10$ql5KIUeEtvZtaqGZMOYvXu8f8zbfQA8yLPk49qiN8yqEeQ4CN9WSa', 3, 'Live', NULL, 0, NULL, '2016-12-07 06:17:23', '2016-12-07 06:17:23'),
-(20, 'sdfsdfsd', 'sdfsdff@dfgdfg.oo', '$2y$10$p5NhGF3x1P1xKdkWO88vpu1HfCg3JIgr7kNMi8YY80wxZKARW/OkK', 2, 'Live', NULL, 0, NULL, '2016-12-08 00:51:16', '2016-12-08 00:51:16'),
-(21, 'fsdfsd', 'sdfdfsd@sdfsdf.pp', '$2y$10$st8Twe6Cpo14wnp.fa9gI.TuxzMaxxNMtNUt8Ge668A/OyOIFeTq6', 2, 'Live', 'NKCHPuEt39KjUiyuuZwyBO2C5X9V83TWp2MMsLkTcfecmZfawbPAbaFkEb6I', 0, NULL, '2016-12-08 01:26:45', '2016-12-08 01:26:50');
+(13, 'child', 'child@gmail.com', '$2y$10$XDFtZWWu5TDQWjrd5qgfsul01g7xygeVolTnuUYfPaNq6yUsoN.PW', 3, 'Live', 'kH0if125330yKcoUqj31EkbBUComAC375VBWZEDBZmBRtJCgjhTtcw0gPr9k', 3, '127.0.0.1', '2016-12-06 04:14:46', '2016-12-11 23:46:50');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
